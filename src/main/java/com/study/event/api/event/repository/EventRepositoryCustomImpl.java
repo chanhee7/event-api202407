36d86c829b84ae23710c0ca0fe6b4012120ae715
chan <chan9505@naver.com>
2024-07-10 18:33:18 +0900

    initial commit

---
+++ src/main/java/com/study/event/api/event/repository/EventRepositoryCustomImpl.java
@@ -0,0 +1,54 @@
+package com.study.event.api.event.repository;
+
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.event.api.event.entity.Event;
+import com.study.event.api.event.entity.QEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class EventRepositoryCustomImpl implements EventRepositoryCustom {
+
+    private final JPAQueryFactory factory;
+
+    @Override
+    public Page<Event> findEvents(Pageable pageable, String sort) {
+
+        // 페이징을 통한 조회
+        List<Event> eventList =  factory
+                .selectFrom(QEvent.event)
+                .orderBy(specifier(sort))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 총 데이터 수 조회
+        Long count = factory
+                .select(QEvent.event.count())
+                .from(QEvent.event)
+                .fetchOne();
+
+        return new PageImpl<>(eventList, pageable, count);
+    }
+
+    // 정렬 조건을 처리하는 메서드
+    private OrderSpecifier<?> specifier(String sort) {
+        switch (sort) {
+            case "date":
+                return QEvent.event.date.desc();
+            case "title":
+                return QEvent.event.title.asc();
+            default:
+                return null;
+        }
+    }
+}
